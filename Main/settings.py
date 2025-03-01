c28fb6012a938d9907d0f163b7b2b10e9bc9fffd
Stan <stan@localhost.localdomain>
2025-02-28 21:07:00 -0700

    moved templates to the root folder

--- Main/settings.py
+++ Main/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates') #templates
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'Main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIRS], #templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
